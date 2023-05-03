fb147f1092b615a9295f74481191a1a7ecbddc5d
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-05-03 20:05:57 +0700

    Server init

---
+++ server.js
@@ -0,0 +1,22 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+  const server = Hapi.server({
+    port: 8080,
+    host: process.env.HOST_ENV !== 'prod' ? 'localhost' : '0.0.0.0',
+  });
+
+  server.route([
+    {
+      method: 'GET',
+      path: '/',
+      handler: () => 'Hello from API',
+    },
+  ]);
+
+  await server.start();
+  console.log(`Hapi.js is running in ${server.info.uri}`);
+};
+
+init();
