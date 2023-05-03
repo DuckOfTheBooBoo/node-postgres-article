cf5ac8ee8183e00058c1b042a3e59ee94e80796f
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-05-03 20:36:58 +0700

    Added DB TABLE

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const CONFIG = {
   POSTGRES_USER: process.env.DB_USER,
   POSTGRES_PASS: process.env.DB_PASS,
   POSTGRES_DB: process.env.DB_NAME,
+  POSTGRES_DB_TABLE: process.env.DB_TABLE_NAME,
   POSTGRES_HOST: 'localhost',
 };
 
