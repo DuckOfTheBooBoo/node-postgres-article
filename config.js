0a744907fcbb9c2b7fc1eb0ff3ad790516a347d0
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-05-16 22:22:08 +0700

    Set default value for POSTGRES_DB_TABLE

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+/* eslint-disable max-len */
 const CONFIG = {
   POSTGRES_USER: process.env.DB_USER,
   POSTGRES_PASS: process.env.DB_PASS,
   POSTGRES_DB: process.env.DB_NAME,
-  POSTGRES_DB_TABLE: process.env.DB_TABLE_NAME,
+  POSTGRES_DB_TABLE: Boolean(process.env.DB_TABLE_NAME) ? process.env.DB_TABLE_NAME : 'articles',
   POSTGRES_HOST: process.env.DB_HOST,
 };
 
